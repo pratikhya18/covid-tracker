52acdeaf805d0e3c2563875c4fa193029e22aa20
pratikhya18 <77261595+pratikhya18@users.noreply.github.com>
2022-02-23 13:35:53 -0600

    log in page updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {ParentComponent} from './state/parent.component';
 import {ChildComponent} from './district/child.component';
 import {CountryComponent} from './country/country.component';
+import {RegistrationComponent} from './registration/registration.component';
 
 
 const routes: Routes = [
+  {path:'register',component: RegistrationComponent},
 {path:'state',component: ParentComponent},
 {path:'district',component: ChildComponent},
 {path:'',component: CountryComponent},
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -12,4 +13,20 @@ export class RegistrationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  LoggedIn(data:any)
+  {
+console.log(data);
+  }
+
+  user: user = {
+      "email":"user123@gmail.com",
+      "password":""
+    };
+
 }
+export  interface user
+{
+  "email":'user123@gmail.com',
+  "password":""
+}
+
